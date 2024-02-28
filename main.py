d293e9aaa0ba58e976786bf6abfb008f47e6b31b
Denys Kavkalo <117307592+DenysKavkalo@users.noreply.github.com>
2024-02-28 15:05:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from validation import is_valid_formula
-from evaluation import print_truth_table
+# Main.py
+
+from table_gen import *
 
 
 def main():
     formula = input("Ingrese la fórmula en lógica proposicional: ").replace(" ", "")
     print("Fórmula ingresada:", formula)  # Impresión de depuración
 
-    # Generar y mostrar la tabla de verdad
-    print("Tabla de Verdad:")
     print_truth_table(formula)
 
 
